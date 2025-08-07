9445337fffb5fe3ebf4f56d540af157ffd4de9b9
richardantony <richard.antony@notetech.com>
2025-08-07 12:58:56 +0530

    small update in menu

--- lib/Models/menu_item_model.dart
+++ lib/Models/menu_item_model.dart
@@ -1,17 +1,21 @@
 class MenuItem {
   final String id;
   final String name;
-  final String icon;
-  final String route;
+  final String? icon;
+  final String? route;
   final String position;
+  final bool active;
+  final bool delete;
   List<MenuItem> children;
 
   MenuItem({
     required this.id,
     required this.name,
-    required this.icon,
-    required this.route,
+    this.icon,
+    this.route,
     required this.position,
+    required this.active,
+    required this.delete,
     this.children = const [],
   });
 
@@ -19,19 +23,11 @@ class MenuItem {
     return MenuItem(
       id: json['id'],
       name: json['name'],
-      position: json['position'],
       icon: json['icon'],
       route: json['route'] ?? '',
+      position: json['position'],
+      active: json['active'],
+      delete: json['delete'],
     );
   }
-
-  // factory MenuItem.fromJson(Map<String, dynamic> json) {
-  //   return MenuItem(
-  //     id: json['id'] ?? '',
-  //     name: json['name'] ?? '',
-  //     icon: json['icon'] ?? '',
-  //     route: json['route'] ?? '',
-  //     position: json['position'] ?? '',
-  //   );
-  // }
 }
